ec4ea2ec46307c4681481539e8e18433aaab5569
aitsihamza <hamzaaitsi65@gmail.com>
2023-03-12 00:36:47 +0100

    fix: removing unused imports

--- demo_keycloak_openfeign/src/main/java/com/example/demo_keycloak_openfeign/configuration/FeignConfig.java
+++ demo_keycloak_openfeign/src/main/java/com/example/demo_keycloak_openfeign/configuration/FeignConfig.java
@@ -4,9 +4,6 @@ import feign.RequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.*;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
